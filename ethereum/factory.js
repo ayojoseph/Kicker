e9a9b58dc9cc7a076d285d75f18e26ce45650f3f
Love Joseph <ayojoseph15@gmail.com>
2018-11-03 17:30:26 -0400

    Factory and React work

---
+++ ethereum/factory.js
@@ -0,0 +1,7 @@
+import web3 from './web3';
+import CampaignFactory from './build/CampaignFactory.json';
+
+const instance = new web3.eth.Contract(
+    JSON.parse(CampaignFactory.interface),'0x350469C19c56c20718ecd8e0809F6ebC7a3d4f24'
+);
+export default instance;
--- pages/show.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default () => {
-  return <h1>This is the new SHOW page. Just adding a bit more here!</h1>;
-};
