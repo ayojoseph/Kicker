499b88fb2402259bac3ed8bbe33f6bb1a078eae3
Love Joseph <ayojoseph15@gmail.com>
2018-11-04 09:09:52 -0500

    front end working

--- pages/index.js
+++ pages/index.js
@@ -3,11 +3,13 @@ import factory from '../ethereum/factory';
 
 
 class CampaignIndex extends Component {
-async componentDidMount() {
-  const campaigns = await factory.methods.getDeployedCampaigns().call()
-  console.log(campaigns);
+  async componentDidMount() {
+    const campaigns = await factory.methods.getDeployedCampaigns().call()
+    console.log(campaigns);
+  }
+  render() {
+    return <div> Campaigns Index!</div>
+  }
 }
-render() {
-  return <div> Campaigns Index!</div>
-}
-}
+
+export default CampaignIndex;
