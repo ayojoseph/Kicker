7b170dee07efafb37154c7f90c11686594e5d283
Unknown <ljoseph@mail.uoguelph.ca>
2018-11-05 16:10:04 -0500

    New campaign form creation

--- pages/campaigns/new.js
+++ pages/campaigns/new.js
@@ -1,11 +1,29 @@
 import React, { Component } from "react";
 import Layout from "../../components/Layout";
+import { Button, Form, Input } from "semantic-ui-react";
 
 class CampaignNew extends Component {
+  state = {
+    minimumCon: ""
+  };
   render() {
     return (
       <Layout>
-        <h1>NEW</h1>
+        <h3>Create a new Campaign</h3>
+        <Form>
+          <Form.Field>
+            <label>Minimum Contribution (100 Wei)</label>
+            <Input
+              label="wei"
+              labelPosition="right"
+              value={this.state.minimumCon}
+              onChange={event =>
+                this.setState({ minimumCon: event.target.value })
+              }
+            />
+          </Form.Field>
+          <Button primary>Create</Button>
+        </Form>
       </Layout>
     );
   }
